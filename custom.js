7f9c5c6aff66d2f74391def3e759f59d5e961a34
Nikolay Danailov <frostblooded@yahoo.com>
2019-10-19 18:33:37 +0300

    Refactor the code a bit so that the colors are in separate variables.g

--- custom.js
+++ custom.js
@@ -28,6 +28,24 @@ var data = {
 };
 
 $(document).ready(function() {
+    var chartBackgroundColors = [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+    ];
+    
+    var chartBorderColor = [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+    ];
+    
     $.each(data, function(i, el){
         var chartId = `chart_${i}`;``
         var $canvas = $(`<canvas id=${chartId}></canvas>`);
@@ -40,25 +58,11 @@ $(document).ready(function() {
         var chartData = {
             labels: labels,
             datasets: [{
-                label: '# of Votes',
+                label: '# of vehicles',
                 data: values,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                borderWidth: 1,
+                backgroundColor: chartBackgroundColors,
+                borderColor: chartBorderColor
             }]
         };
 
