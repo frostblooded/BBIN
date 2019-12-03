bcfbbe4331a41ebecfd5ef310a55c4947ddb629b
Nikolay Danailov <frostblooded@yahoo.com>
2019-12-03 20:30:35 +0200

    Make the processing script easier to configure which files to use. Also start using median for data summarization.

--- data_location_finder.rb
+++ data_location_finder.rb
@@ -1,13 +1,15 @@
 require 'csv'
-require 'byebug'
-require 'net/http'
 require 'json'
-require 'pr_geohash'
 
 HASHES = {
   sofia: %w(sx8e sx8d),
   plovdiv: %w(sx3x),
   vidin: %w(sxb4r sxb62 sxb4p sxb60)
+}
+
+FILES_TO_PROCESS = {
+  'data/airtube-data-BG-2018.csv' => 'processed_data/airtube_data_by_month_2018.csv',
+  'data/airtube-data-BG-2019.csv' => 'processed_data/airtube_data_by_month_2019.csv',
 }
 
 def get_city hash
@@ -22,67 +24,78 @@ def get_city hash
   nil
 end
 
+# Return the month as a single digit
 def get_month date
-  date.split('-')[1]
+  month_two_digits = date.split('-')[1]
+  return month_two_digits[1].to_s if month_two_digits[0] == '0'
+  month_two_digits
 end
 
-puts "Loading CSV into memory..."
-csv = CSV.read('data/airtube-data-BG-2018.csv')
-puts "CSV loaded into memory. Start processing data..."
+def median array
+  sorted = array.sort
+  len = sorted.length
+  (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+end
 
-by_city_by_date = {}
+def process_data input_file, output_file
+  puts "Start processing data from #{input_file}"
+  puts "Loading file into memory..."
+  csv = CSV.read(input_file)
+  puts "File loaded into memory. Start processing data..."
 
-csv_rows_total = csv.size
-csv_rows_done = 0
+  by_city_by_date = {}
 
-csv[1..].each do |row|
-  csv_rows_done += 1
+  csv_rows_total = csv.size
+  csv_rows_done = 0
 
-  if csv_rows_done % 100_000 == 0
-    percent = 100 * csv_rows_done / csv_rows_total
-    puts "Adding to by_city_by_date hash: Done #{csv_rows_done} out of #{csv_rows_total} (#{percent}%)"
+  csv[1..].each do |row|
+    csv_rows_done += 1
+
+    if csv_rows_done % 100_000 == 0
+      percent = 100 * csv_rows_done / csv_rows_total
+      puts "Adding to by_city_by_date hash: Done #{csv_rows_done} out of #{csv_rows_total} (#{percent}%)"
+    end
+
+    next unless row[1]
+
+    city = get_city row[1]
+    next if city.nil?
+
+    month = get_month row[0]
+    p10 = row[2]
+    p25 = row[3]
+    temperature = row[4]
+
+    by_city_by_date[city] = {} unless by_city_by_date.key? city
+    by_city_by_date[city][month] = [] unless by_city_by_date[city].key? month
+    by_city_by_date[city][month] << [p10, p25, temperature]
   end
 
-  next unless row[1]
+  puts "Start writing into #{output_file}"
 
-  city = get_city row[1]
-  next if city.nil?
+  CSV.open(output_file, 'wb') do |new_csv|
+    new_csv << %w(city month p10 p2.5 temp)
 
-  month = get_month row[0]
-  p10 = row[2]
-  p25 = row[3]
-  temperature = row[4]
+    by_city_by_date.each do |current_city, city_data|
+      puts "Writing data for city #{current_city}..."
 
-  by_city_by_date[city] = {} unless by_city_by_date.key? city
-  by_city_by_date[city][month] = [] unless by_city_by_date[city].key? month
-  by_city_by_date[city][month] << [p10, p25, temperature]
-end
+      city_data.each do |date, date_data|
+        p1_arr = []
+        p2_arr = []
+        temp_arr = []
 
-CSV.open('processed_data/airtube_data_by_month_2018.csv', 'wb') do |new_csv|
-  new_csv << %w(city month p10 p2.5 temp)
+        date_data.each do |instance|
+          p1_arr << instance[0].to_i
+          p2_arr << instance[1].to_i
+          temp_arr << instance[2].to_i
+        end
 
-  by_city_by_date.each do |current_city, city_data|
-    puts "Writing data for city #{current_city}..."
-
-    city_data.each do |date, date_data|
-      date_size = date_data.size
-
-      p1_sum = 0
-      p2_sum = 0
-      temp_sum = 0
-
-      date_data.each do |instance|
-        p1_sum += instance[0].to_i
-        p2_sum += instance[1].to_i
-        temp_sum += instance[2].to_i
+        new_csv << [current_city, date, median(p1_arr), median(p2_arr), median(temp_arr)]
       end
-
-      p1_sum /= date_size
-      p2_sum /= date_size
-      temp_sum /= date_size
-
-      new_csv << [current_city, date, p1_sum, p2_sum, temp_sum]
     end
   end
 end
 
+FILES_TO_PROCESS.each do |input_file, output_file|
+  process_data input_file, output_file
+end
